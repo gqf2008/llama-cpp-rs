ff57e7b27c39c7d9a20c93b8b5a846eae245e542
高庆丰 <gao.qingfeng@gmail.com>
2024-08-28 13:14:13 +0800

    Update main.rs

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -185,7 +185,7 @@ fn main() -> Result<()> {
                 .with_context(|| "unable to create the llama_context")
                 .unwrap();
             for _ in 0..10 {
-                ctx.clear_kv_cache_seq(0, Some(2048), None);
+                ctx.clear_kv_cache();
                 let out = ctx.forward(prompt.clone(), n_len).unwrap();
 
                 println!("{out}");
